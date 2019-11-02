15421cc2fe88d6185ba24100c62ea5014c328652
Mathieu <mathieu.huot@gmail.com>
2019-11-01 23:23:59 -0400

    Working on default answer

--- script/index.js
+++ script/index.js
@@ -14,73 +14,58 @@ showClue('Q3', 'C');
 showClue('Q4', 'D');
 showClue('Q5', 'E');
 
+//Result code
 //DOM util function
 const getElement = classOrId => document.querySelector(classOrId);
+const elem = element => document.createElement(element);
 
-//Result code
 const showResult = event => {
     event.preventDefault();
     window.location = '#your-result';
-
-    const result1 = getElement('#result-1');
-    const result2 = getElement('#result-2');
-    const result3 = getElement('#result-3');
-    const result4 = getElement('#result-4');
-    const result5 = getElement('#result-5');
+    const result = getElement('#result');
+    if(result.hasChildNodes()) result.innerHTML = '';
+    const percent = getElement('#percentage');
+    if(percent.hasChildNodes()) percent.innerHTML = '';
+    var score = 0;
+    const fragment = document.createDocumentFragment();
     
     const form = getElement('.form');
-    const success = (where, Q, value) => where.innerHTML = `
-    <h3>Your answer to ${Q}</h3>
-    <p>Congratulation "${value}" is the right answer!</p>
-    `;
-    const wrong = (where, Q, value) => where.innerHTML = `
-    <h3>Your answer to ${Q}</h3>
-    <p>Sorry "${value}" is the wrong answer.</p>
-    `;
-    const Q1 = getElement('input[name=Q1]:checked');
-    const Q2 = getElement('input[name=Q2]:checked');
-    const Q3 = getElement('input[name=Q3]:checked');
-    const Q4 = getElement('input[name=Q4]:checked');
-    const Q5 = getElement('input[name=Q5]:checked');
-          
-        switch(Q1.value) {
-            case '7':
-                success(result1, 'Question 1', Q1.value);
-                break;
-            default:
-                wrong(result1, 'Question 1', Q1.value); 
-        }
-        switch(Q2.value) {
-            case 'Barry Nelson':
-                success(result2, 'Question 2', Q2.value);
-                break;
-            default:
-                wrong(result2, 'Question 2', Q2.value); 
-        }
-        switch(Q3.value) {
-            case '7':
-                success(result3, 'Question 3', Q3.value);
-                break;
-            default:
-                wrong(result3, 'Question 3', Q3.value); 
-        }
-        switch(Q4.value) {
-            case 'Diamonds Are Forever':
-                success(result4, 'Question 4', Q4.value);
-                break;
-            default:
-                wrong(result4, 'Question 4', Q4.value); 
-        }
-        switch(Q5.value) {
-            case 'Paul McCartney':
-                success(result5, 'Question 5', Q5.value);
-                break;
-            default:
-                wrong(result5, 'Question 5', Q5.value); 
-        }
-        form.reset();
+    let answer1 = getElement('[name=Q1]:checked');
+    let answer2 = getElement('[name=Q2]:checked');
+    let answer3 = getElement('[name=Q3]:checked');
+    let answer4 = getElement('[name=Q4]:checked');
+    let answer5 = getElement('[name=Q5]:checked');
+    const answers = [
+        answer1,
+        answer2,
+        answer3,
+        answer4,
+        answer5
+    ]
+    answers.forEach(a => {
+        const right = a.dataset.answer === a.value;
+        const h3 = elem('h3');
+        const verdict = elem('p');
+        h3.textContent = `Verifying your answer to ${a.dataset.qtitle}`;
+        verdict.textContent = right ? `Congratulation, "${a.value}" is the right answer!` : `Sorry "${a.value}" is not the right answer. Try again.`;
+        verdict.style.color = right ? 'green' : 'red';
+        if(right) score += 20;
+        fragment.appendChild(h3);
+        fragment.appendChild(verdict);
+        result.appendChild(fragment);
+    });
+    const scoreNode = document.createTextNode(`${score}%`);
+    fragment.appendChild(scoreNode);
+    percent.appendChild(fragment);
+    form.reset();
 }
 (function() {
     const resultBtn = document.getElementById('submit-answers');
     resultBtn.addEventListener('click', showResult);
+})();
+(function() {
+    const resetBtn = document.getElementById('reset-btn');
+    resetBtn.addEventListener('click', () => {
+        window.location.reload();
+    });
 })();
